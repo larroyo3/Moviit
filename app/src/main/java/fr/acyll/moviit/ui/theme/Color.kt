535359e8652a6ddf277be4d4ba3e6073cf5bce1b
lucas <lucas.arroyo70@gmail.com>
2024-02-09 10:43:11 +0100

    add color theme

--- app/src/main/java/fr/acyll/moviit/ui/theme/Color.kt
+++ app/src/main/java/fr/acyll/moviit/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val CyanCornflowerBlue = Color(0xFF1989AC)
+val BlueSapphire = Color(0xFF13667F)
+
+val OldLace = Color(0xFFFEF4E8)
+
+val CharlestonGreen = Color(0xFF2A2A2A)
+
+val Black = Color(0xFF111111)
--- app/src/main/java/fr/acyll/moviit/ui/theme/Theme.kt
+++ app/src/main/java/fr/acyll/moviit/ui/theme/Theme.kt
@@ -22,19 +22,20 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = CyanCornflowerBlue,
+    onPrimary = BlueSapphire,
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    secondary = CharlestonGreen,
+    onSecondary = Black,
+
+    //tertiary = Pink40,
+    //onTertiary = Color.White,
+
+    background = OldLace,
+    onBackground = CharlestonGreen,
+
+    surface = OldLace,
+    onSurface = CharlestonGreen,
 )
 
 @Composable
