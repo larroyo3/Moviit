9d8efb88535a5946e1e3ba09a452a2ac866401ad
lucas <lucas.arroyo70@gmail.com>
2024-02-13 23:50:40 +0100

    update ui

--- app/src/main/java/fr/acyll/moviit/features/main/home/HomeScreen.kt
+++ app/src/main/java/fr/acyll/moviit/features/main/home/HomeScreen.kt
@@ -3,11 +3,14 @@ package fr.acyll.moviit.features.main.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import fr.acyll.moviit.R
 import fr.acyll.moviit.components.NoActionBarScreenContainer
 import fr.acyll.moviit.ui.theme.MoviitTheme
 
@@ -25,7 +28,11 @@ fun ScreenContent() {
             .fillMaxSize()
             .padding(16.dp)
     ) {
-        Text(text = "coucou home")
+        // TODO : mettre vrai typo
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
 
--- app/src/main/java/fr/acyll/moviit/features/onboarding/auth/AuthScreen.kt
+++ app/src/main/java/fr/acyll/moviit/features/onboarding/auth/AuthScreen.kt
@@ -1,11 +1,16 @@
 package fr.acyll.moviit.features.onboarding.auth
 
+import android.widget.ImageView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,6 +39,31 @@ fun ScreenContent(
           .fillMaxSize()
           .padding(16.dp)
     ) {
+        // TODO : mettre vrai typo
+        Text(
+            text = stringResource(id = R.string.app_name),
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Spacer(modifier = Modifier.weight(0.3F))
+
+        Image(
+            painter = painterResource(id = R.drawable.illustration_movie_set),
+            contentDescription = null,
+        )
+
+        // TODO : update text style
+        Spacer(modifier = Modifier.padding(16.dp))
+        Text(
+            text = stringResource(id = R.string.auth_title),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.padding(8.dp))
+        Text(
+            text = stringResource(id = R.string.auth_desc),
+            style = MaterialTheme.typography.bodyLarge
+        )
+
 
         Spacer(modifier = Modifier.weight(1f))
         PrimaryButton(
